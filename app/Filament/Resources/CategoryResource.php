f17dcac215f7480e75864efab10d379588fa235f
PeYoT <putraananta08@gmail.com>
2025-01-21 00:11:51 +0700

    change logo category

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -17,7 +17,7 @@ class CategoryResource extends Resource
 {
     protected static ?string $model = Category::class;
 
-    protected static ?string $navigationIcon = 'heroicon-s-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-s-inbox-stack';
 
     public static function form(Form $form): Form
     {
